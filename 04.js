ac70286983f051138316028b3f09ebb6228fb94e
MOHAMED NAUFAL <mnaufalmz@gmail.com>
2022-06-25 19:16:19 +0530

    Added 4th exercise

---
+++ 04.js
@@ -0,0 +1,13 @@
+// to rotate the string 'javascript' in right direction by periodically removing one letter from the end of the string and attaching it to the front.
+
+function animate_string(id) 
+{
+    const element = document.getElementById(id);
+    const textNode = element.childNodes[0]; // assuming no other children
+    let text = textNode.data;
+
+setInterval(() => {
+ text = text[text.length - 1] + text.substring(0, text.length - 1);
+  textNode.data = text;
+}, 100);
+}
