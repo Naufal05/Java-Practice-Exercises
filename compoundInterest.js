5d2a215ee694ebc02fbfd654f9b550b52aeaac8c
MOHAMED NAUFAL <mnaufalmz@gmail.com>
2022-07-06 03:33:14 +0530

    compound intrest

---
+++ compoundInterest.js
@@ -0,0 +1,5 @@
+function compoundInt(principle, rate, time, n) {
+  var interest = principle * Math.pow(1 + rate / n, n * time);
+  return principle + interest;
+}
+console.log(compoundInt(20000, 5, 2, 2));
